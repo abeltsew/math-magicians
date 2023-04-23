c7d5573157b871f4c801c669598242c48754ff3c
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-04-23 20:26:07 +0300

    Make keys DRY

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -26,34 +26,23 @@ const Calculator = () => {
           {obj?.next}
         </p>
       </div>
-      <ul className="row1">
-        <Keys btn="AC" clickHandler={() => handleClick('AC')} />
-        <Keys btn="+/-" clickHandler={() => handleClick('+/-')} />
-        <Keys btn="%" clickHandler={() => handleClick('%')} />
-        <Keys btn="÷" cName="yellow" clickHandler={() => handleClick('÷')} />
-      </ul>
-      <ul className="row2">
-        <Keys btn="7" clickHandler={() => handleClick('7')} />
-        <Keys btn="8" clickHandler={() => handleClick('8')} />
-        <Keys btn="9" clickHandler={() => handleClick('9')} />
-        <Keys btn="x" cName="yellow" clickHandler={() => handleClick('x')} />
-      </ul>
-      <ul className="row3">
-        <Keys btn="4" clickHandler={() => handleClick('4')} />
-        <Keys btn="5" clickHandler={() => handleClick('5')} />
-        <Keys btn="6" clickHandler={() => handleClick('6')} />
-        <Keys btn="-" cName="yellow" clickHandler={() => handleClick('-')} />
-      </ul>
-      <ul className="row4">
-        <Keys btn="1" clickHandler={() => handleClick('1')} />
-        <Keys btn="2" clickHandler={() => handleClick('2')} />
-        <Keys btn="3" clickHandler={() => handleClick('3')} />
-        <Keys btn="+" cName="yellow" clickHandler={() => handleClick('+')} />
-      </ul>
-      <ul className="row5">
+      {[
+        ['AC', '+/-', '%', '÷'],
+        ['7', '8', '9', 'x'],
+        ['4', '5', '6', '-'],
+        ['1', '2', '3', '+'],
+      ].map((row) => (
+        <ul key={`row${row[0]}`}>
+          {row.map((btn) => (
+            <Keys key={btn} btn={btn} clickHandler={() => handleClick(btn)} />
+          ))}
+        </ul>
+      ))}
+      <ul>
         <Keys btn="0" cName="zero" clickHandler={() => handleClick('0')} />
-        <Keys btn="." clickHandler={() => handleClick('.')} />
-        <Keys btn="=" cName="yellow" clickHandler={() => handleClick('=')} />
+        {['.', '='].map((btn) => (
+          <Keys key={btn} btn={btn} clickHandler={() => handleClick(btn)} />
+        ))}
       </ul>
     </div>
   );
