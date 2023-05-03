f445f9b52043763d979768e0e2c2b26b4a30f818
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-05-03 15:39:49 +0300

    Add Calculate Functionality Test

--- src/__test__/operate.test.js
+++ src/__test__/operate.test.js
@@ -1,29 +1,59 @@
-import operate from './operate';
+import calculate from '../components/calculate';
+import operate from '../components/operate';
 
-test('ADD : 1 + 2 to equal 3', () => {
-  expect(operate('1', '2', '+')).toBe('3');
+describe('Should be abel to do basic operations', () => {
+  test('ADD : 1 + 2 to equal 3', () => {
+    expect(operate('1', '2', '+')).toBe('3');
+  });
+
+  test('SUBTRACT : 8 - 3 to equal 5', () => {
+    expect(operate('8', '3', '-')).toBe('5');
+  });
+
+  test('MULTIPLY : 4 x 2 to equal 8', () => {
+    expect(operate('4', '2', 'x')).toBe('8');
+  });
+
+  test('DIVIDE : 10 ÷ 5 to equal 2', () => {
+    expect(operate('10', '5', '÷')).toBe('2');
+  });
+
+  test('DIVIDE BY ZERO : 7 ÷ 0 to equal Undefined', () => {
+    expect(operate('7', '0', '÷')).toBe("Can't divide by 0.");
+  });
+
+  test('MODULUS :  19 % 6 to equal 1', () => {
+    expect(operate('19', '6', '%')).toBe('1');
+  });
+
+  test('MODULUS BY ZERO : 7 % 0 to equal Undefined', () => {
+    expect(operate('7', '0', '%')).toBe(
+      "Can't find modulo as can't divide by 0.",
+    );
+  });
 });
 
-test('SUBTRACT : 8 - 3 to equal 5', () => {
-  expect(operate('8', '3', '-')).toBe('5');
-});
+describe('it should apply the correct operation', () => {
+  it('should clear the values', () => {
+    const clear = calculate({ total: 10 }, 'AC');
 
-test('MULTIPLY : 4 x 2 to equal 8', () => {
-  expect(operate('4', '2', 'x')).toBe('8');
-});
+    expect(Number(clear.total)).toBe(0);
+  });
 
-test('DIVIDE : 10 ÷ 5 to equal 2', () => {
-  expect(operate('10', '5', '÷')).toBe('2');
-});
+  it('should sum the values', () => {
+    const sum = calculate({ total: 10, next: 2, operation: '+' }, '=');
 
-test('DIVIDE BY ZERO : 7 ÷ 0 to equal Undefined', () => {
-  expect(operate('7', '0', '÷')).toBe("Can't divide by 0.");
-});
+    expect(Number(sum.total)).toBe(12);
+    expect(sum).toEqual({
+      total: '12',
+      next: null,
+      operation: null,
+    });
+  });
 
-test('MODULUS :  19 % 6 to equal 1', () => {
-  expect(operate('19', '6', '%')).toBe('1');
-});
+  it('should handle decimal or .(dot)', () => {
+    const dot = calculate({ total: 0, next: null, operation: null }, '.');
 
-test('MODULUS BY ZERO : 7 % 0 to equal Undefined', () => {
-  expect(operate('7', '0', '%')).toBe("Can't find modulo as can't divide by 0.");
+    expect(dot.next).toBe('0.');
+  });
 });
